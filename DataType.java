40380da6f760681ed5520421fff62159e35fa730
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-11 19:15:14 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,34 @@
+ /**
+  * DataType
+  */
+
+   //The variables are containers for storing data.
+// primitive = self  derived----- boolean,char,float,int,shor,double.
+ public class DataType {
+ 
+    public static void main(String[] args) {
+        boolean b1=true;
+        char ch='G';
+        byte by= 120;
+        float f1 =2.8f;
+        int intData =10;
+        double d1= 122877;
+        System.out.println(b1);
+        System.out.println(ch);
+        System.out.println(by);
+        System.out.println(intData);
+        System.out.println(f1);
+        System.out.println(d1);
+
+        // non-primitive= derived
+        // String,arry,classes etc.
+        String st="Dimpal";
+        System.out.println(st);
+        // array
+        int result[]={12,10,30};
+        System.out.println(result[0]);
+    }
+ }
+ 
+
+ 
