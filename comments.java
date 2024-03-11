131e4f73849a5cb89d03490f8ab83042e4f98d81
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-11 18:13:19 +0530

    Add files via upload

---
+++ comments.java
@@ -0,0 +1,18 @@
+ // Single line comments.
+ class comments {
+    public static void main(String[] args) {
+        // Single line comment.
+        int firstNumber =10;// firstNumber is a variable with value 10.
+        /*Multiline comment. */
+        int secondNumber=12;/*SecondNumber is a variable with value 12 */ 
+        /** Documentation comments
+         * @author Dimpal
+         * @since 11 march 2024 
+         */
+        System.out.println(firstNumber+secondNumber);
+    }
+}
+
+
+
+
