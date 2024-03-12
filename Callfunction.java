0520efee22d3d2edf9fd01df51bfec5b0bbc1ff0
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-12 15:21:51 +0530

    Add files via upload

---
+++ Callfunction.java
@@ -0,0 +1,16 @@
+/**
+ * Callfunction
+ */
+
+//Function are set of code.
+
+
+ public class Callfunction {
+
+    static void Dimpal(){
+        System.out.println("welocme to Dimpal");
+    }
+    public static void main(String[] args) {
+        Dimpal();
+    }
+}
